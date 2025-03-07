db1e485c1dbaa6f5abfa83ffd712fc03ac405c41
josealonso <jose.alonso.programmer@gmail.com>
2025-03-07 17:29:04 +0100

    Returning a ResponseEntity type gives control over the response status, headers, and body.

--- src/main/java/com/example/demo/controller/IssuesController.java
+++ src/main/java/com/example/demo/controller/IssuesController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import com.example.demo.model.Issue;
 import com.example.demo.repository.IssueRepositoryImpl;
 
 @Controller
@@ -13,8 +17,11 @@ public class IssuesController {
     IssueRepositoryImpl issueRepositoryImpl;
 
     @GetMapping("/issues")
-    public String getIssues() {
-        return issueRepositoryImpl.findAll().toString();
+    public ResponseEntity<List<Issue>> getIssues() {
+        return ResponseEntity.ok()
+        .header("Content-Type", "application/json")
+        .body(issueRepositoryImpl.findAll());  // body() method is terminal and returns the built ResponseEntity, so .build() is not needed and would cause a compilation error.
+        // .build(); 
     }
 
 }
