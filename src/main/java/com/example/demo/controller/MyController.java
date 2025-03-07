8dc612465fb83abc90ce1a4b6bd07e2c9a5ee327
josealonso <jose.alonso.programmer@gmail.com>
2025-03-07 13:41:35 +0100

    How to pass query parameters to a Thymeleaf template.

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
@@ -9,6 +11,13 @@ public class MyController {
     @GetMapping("/index")
     public String hello() {
         return "index";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, 
+            Model model) {
+        model.addAttribute("name", name); // Add the name parameter to the model
+        return "greeting";
     }
     
 }
